65e1d0f68efee28727058ad5dcee9201ef4e9f84
imperionite <lr.aimperial@mmdc.mcl.edu.ph>
2025-02-20 00:45:11 +0800

    :fire: (frontend) edit folder

--- frontend/src/atoms/store.js
+++
@@ -1,8 +0,0 @@
-import { atomWithStorage } from 'jotai/utils'
-import { atom } from 'jotai';
-
-export const jwtAtom = atomWithStorage('jwtAtom', { access: '' })
-
-export const expAtom = atomWithStorage('expAtom', 0)
-
-export const accessTokenAtom = atom(null);
--- frontend/src/services/queryKeyFactory.js
+++
@@ -1,8 +0,0 @@
-// keys for tanstack query and muatations
-export const userKeys = {
-    all: ['users'],
-    lists: () => [...userKeys.all, 'list'],
-    list: (filters) => [...userKeys.lists(), { filters }],
-    details: () => [...userKeys.all, 'detail'],
-    detail: (id) => [...userKeys.details(), id],
-  }
